67dc27b6974a092c84e30e0a5c098b4f45d9be47
Ojay95 <iamojay89@gmail.com>
2026-07-06 17:03:45 +0100

    Update layout favicon to reference public/favicon.png

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#2563eb" />
       </head>
